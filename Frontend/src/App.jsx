060205cadd90c99c6a2da4e1ff4b03c84f0aa40b
Kumar Anuj <seneva.981@gmail.com>
2025-01-24 12:19:40 +0530

    Twelfth Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -42,20 +42,30 @@ import UserSignup from "./pages/UserSignup"
 import CaptainLogin from "./pages/CaptainLogin"
 import CaptainSignup from "./pages/CaptainSignup"
 import Home from "./pages/Home";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
+import { UserLogout } from "./pages/UserLogout";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={ <Start/> } />
-        <Route path="/login" element={ <UserLogin/> } />
-        <Route path="/signup" element={ <UserSignup/> } />
-        <Route path="/captain-login" element={ <CaptainLogin/> } />
-        <Route path="/captain-signup" element={ <CaptainSignup/> } />
-        <Route path="/home" element={ <Home/> } />
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          }
+        />
+        <Route path="/users/logout" element={<UserProtectWrapper><UserLogout/></UserProtectWrapper>}/>
       </Routes>
     </div>
-  )
+  );
 };
 
 export default App;
--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -3,18 +3,19 @@ import React, { createContext, useState } from "react";
 export const UserDataContext = createContext();
 
 const UserContext = ({ children }) => {
-
-    const [user, setUser] = useState({
-        email: "",
-        fullName: {
-            firstName: "",
-            lastName: ""
-        }
-    })
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
 
   return (
     <div>
-      <UserDataContext.Provider value={[user, setUser]}>{children}</UserDataContext.Provider>
+      <UserDataContext.Provider value={{user, setUser}}>
+        {children}
+      </UserDataContext.Provider>
     </div>
   );
 };
