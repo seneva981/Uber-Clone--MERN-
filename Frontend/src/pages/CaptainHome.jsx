e3ee5fb0eba41e3bb6d79bb0d09b3955072d7ef4
Kumar Anuj <seneva.981@gmail.com>
2025-04-14 22:08:51 +0530

    Sixteenth Commit (Captain Home Screen UI Completed)

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
+import RidePopUp from "../components/RidePopUp";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import ConfirmRidePopUp from "../components/ConfirmRidePopUp";
 
 const CaptainHome = () => {
-  return (
-    <div>CaptainHome</div>
-  )
-}
+  const [ridePopupPanel, setRidePopupPanel] = React.useState(true);
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] =
+    React.useState(false);
+  const ridePopupPanelRef = useRef(null);
+  const confirmRidePopupPanelRef = useRef(null);
 
-export default CaptainHome
+  useGSAP(
+    function () {
+      if (ridePopupPanel) {
+        gsap.to(ridePopupPanelRef.current, { transform: "translateY(0)" });
+      } else {
+        gsap.to(ridePopupPanelRef.current, { transform: "translateY(100%)" });
+      }
+    },
+    [ridePopupPanel]
+  );
+
+  useGSAP(
+    function () {
+      if (confirmRidePopupPanel) {
+        gsap.to(confirmRidePopupPanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(confirmRidePopupPanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [confirmRidePopupPanel]
+  );
+
+  return (
+    <div className="h-screen">
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
+        <img
+          className="w-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          alt=""
+        />
+        <Link
+          to="/home"
+          className="h-10 w-10 bg-white flex items-center justify-center rounded-full"
+        >
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className="h-3/5">
+        <img
+          className="h-full w-full object-cover"
+          src="https://kenh14cdn.com/thumb_w/660/2019/11/20/img0107-15742433645641030281314-15742433836991153843682-15742445447481853654987.png"
+          alt=""
+        />
+      </div>
+      <div className="h-2/5 p-6">
+        <CaptainDetails></CaptainDetails>
+      </div>
+      <div
+        ref={ridePopupPanelRef}
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+      >
+        <RidePopUp
+          setRidePopupPanel={setRidePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+        ></RidePopUp>
+      </div>
+      <div
+        ref={confirmRidePopupPanelRef}
+        className="fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+      >
+        <ConfirmRidePopUp
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+          setRidePopupPanel={setRidePopupPanel}
+        ></ConfirmRidePopUp>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainHome;
