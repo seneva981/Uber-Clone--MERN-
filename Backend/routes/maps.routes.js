f03ff6f79f322e68a1544010cfbc31fabbf57f11
Kumar Anuj <seneva.981@gmail.com>
2025-04-19 00:42:56 +0530

    Eighteenth Commit

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/auth.middleware");
 const mapController = require("../controllers/maps.controller");
-const {query} = require("express-validator")
+const { query } = require("express-validator");
 
 const router = express.Router();
 
-
-router.get("/get-coordinates", query("address").isString().isLength({ min: 3 }), authMiddleware.authUser, mapController.getCoordinates)
+router.get(
+  "/get-coordinates",
+  query("address").isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getCoordinates
+);
+router.get(
+  "/get-distance-time",
+  query("origin").isString().isLength({ min: 3 }),
+  query("destination").isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getDistanceTime
+);
 
 module.exports = router;
